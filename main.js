3bd645d07210fb057108767337920c7b43143b22
hhhh8815 <55180599+hhhh8815@users.noreply.github.com>
2020-07-05 13:00:25 +0800

    練習

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 console.log("我是JS!!!");
+// DOM 文件物件模式
+// 文件.透過 ID 搜尋元素("ID 名稱").插入 HTML = "文字訊息";
+document.getElementById("box1").innerHTML = "哈囉~! javaScripe";
+
+// JavaScript variables are containers for storing data values.
+// JavaScript變數是用於存儲數據值的容器。
+var x = 10;         //整數
+var y = 1.5;        //小數點
+var z ="文字"       //字串
+var a =true         //不寧值 ture 是, false 否,
+
+console.log(x);
+
+// 加 減 乘 除 餘數 + - * / %
+
+x = x - 2 ;
+
+console.log(x);
+
+// 目前版本 HTML5，CSS3，JS ES6
+// ++ 遞增1 -- 遞減1
+var e = 7;
+e++;
+console.log(e);
+
+// ES6 指數符號 **
+var f = 5;
+f = f**3;
+console.log(f);
